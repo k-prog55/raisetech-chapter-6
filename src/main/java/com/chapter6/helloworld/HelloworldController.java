e98ba73f2f549ac62aeef1fe74bd3890bdeb4c6b
k-prog55 <keianzai@keinoMacBook-Air.local>
2023-07-30 14:41:45 +0900

    時刻の表示を追加

--- src/main/java/com/chapter6/helloworld/HelloworldController.java
+++ src/main/java/com/chapter6/helloworld/HelloworldController.java
@@ -1,8 +1,12 @@
 package com.chapter6.helloworld;
 
+import ch.qos.logback.core.model.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 @RestController
@@ -45,4 +49,16 @@ public class HelloworldController {
         Name name = new Name("masataka", "yoshida", 30);
         return name;
     }
+
+    //現在時刻を表示
+    @GetMapping("/LocalDateTime")
+    public LocalDateTime getCurrentDatetime() {
+        return LocalDateTime.now();
+    }
+
+    @GetMapping("/ZonedDateTime")
+    public ZonedDateTime getCurrentDateTime(){
+        return ZonedDateTime.now();
+    }
+
 }
